1c7376e5bbec091b95523e2fd9ab5dc50bba00b8
Yen <112124817+Yepham@users.noreply.github.com>
2022-10-04 21:47:13 +0200

    Update streamlit_app.py
    add some code for numpy and random chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,24 @@
 # Create the main python file
 import streamlit
 
+import numpy as np
+import time
+
 streamlit.title('Hello, Streamlit!!')
+
+# Get some data.
+data = np.random.randn(10, 2)
+
+# Show the data as a chart.
+chart = st.line_chart(data)
+
+# Wait 1 second, so the change is clearer.
+time.sleep(1)
+
+# Grab some more data.
+data2 = np.random.randn(10, 2)
+
+# Append the new data to the existing chart.
+chart.add_rows(data2)
+
+
