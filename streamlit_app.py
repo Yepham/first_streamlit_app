36114aa5c63d188be2bca543106c86e1826abc2e
Yen <112124817+Yepham@users.noreply.github.com>
2022-10-04 21:52:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 hide_menu_style = """
         <style>
-        #MainMenu {visibility: hidden;}
+        MainMenu {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
