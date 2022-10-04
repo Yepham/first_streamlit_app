2323932330aeddde08d61cdfaa10c8f10bb575da
Yen <112124817+Yepham@users.noreply.github.com>
2022-10-04 22:00:59 +0200

    Update streamlit_app.py
    add pandas fruit macros

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Create the main python file
 import streamlit
+import pandas
 
 
 streamlit.title('Hello, Streamlit 🥑 🐔')
@@ -10,4 +11,11 @@ streamlit.text('Warm waffel')
 streamlit.text('Warm drinks')
 streamlit.text('🥗 Warm vegetable')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
+
 streamlit.header('🥝🍇 I built my own menu')
+
+
