2a347902b5a33301aa6ab9f639b74f08b32873da
Yen <112124817+Yepham@users.noreply.github.com>
2022-10-04 22:03:09 +0200

    Update streamlit_app.py
    add some input

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,19 @@ streamlit.text('Warm waffel')
 streamlit.text('Warm drinks')
 streamlit.text('🥗 Warm vegetable')
 
+streamlit.header('🥝🍇 I built my own menu')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
 
 
-streamlit.header('🥝🍇 I built my own menu')
+
 
 
